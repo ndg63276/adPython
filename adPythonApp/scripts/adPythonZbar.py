64da98ad96d20d5b2d893a7a4ee64492124ecfc4
Ulrik Pedersen <Ulrik.Pedersen@diamond.ac.uk>
2013-10-31 11:44:45 +0000

    adPythonZbar: added a generic barcode class with the intention to replace the implementation specific zbar class.

--- adPythonApp/scripts/adPythonZbar.py
+++ adPythonApp/scripts/adPythonZbar.py
@@ -27,6 +27,10 @@ class BarCodeSymbol:
         and converts it into a shape that OpenCV can treat as points in a polygon.'''
         points = numpy.array(points, numpy.int32)
         self.polygon = points.reshape((-1, 1, 2))
+    
+    @staticmethod
+    def quality_sorted(obj_list):
+        return sorted( obj_list, key=lambda x: x.quality, reverse=True )
         
 class BarCodeDecoder:
     '''BarDecoder base class to provide a common interface for 
@@ -39,7 +43,7 @@ class BarCodeDecoder:
         s = "< BarDecoder symbols: %s >" % str(self.symbols)
         return s
         
-    def decode(self, array):
+    def decode(self, array, threshold = 50):
         '''Decode the barcode symbols from a 2D array
         The input array should be a numpy 2D array of unsigned 8 bpp elements.
         Returns the number of symbols decoded from the array.'''
@@ -52,7 +56,7 @@ class ZBarDecoder(BarCodeDecoder):
         self._scanner = zbar.ImageScanner()
         self._scanner.parse_config('enable')
     
-    def decode(self, array):
+    def decode(self, array, threshold = 50):
         self.symbols = []
         zimg = zbar.Image( array.shape[0], array.shape[1], 'Y800', array.tostring() )
         self._scanner.scan(zimg)
@@ -63,24 +67,94 @@ class ZBarDecoder(BarCodeDecoder):
             symbol.quality = int(zb_symbol.quality)
             symbol.points_to_polygon(zb_symbol.location)
             self.symbols.append(symbol)
+        self.symbols = BarCodeSymbol.quality_sorted( self.symbols )
         return len(self.symbols)
 
 class DmtxDecoder(BarCodeDecoder):
     '''Use the pydmtx - python bindings for libdmtx to decode Data Matrix barcodes'''
     def __init__(self, logger='DmtxDecoder'):
         BarCodeDecoder.__init__(self, logger)
-        self._dm_read = pydmtx.DataMatrix()
+        self._dm_read = pydmtx.DataMatrix(max_count = 1, timeout = 5000)
         
-    def decode(self, array):
+    def decode(self, array, threshold = 50):
         self.symbols = []
-        self._dm_read.decode( array.shape[0], array.shape[1], buffer(array))
+        self._dm_read.decode( array.shape[0], array.shape[1], 
+                              buffer(array), threshold=threshold)
         num_hits = self._dm_read.count()
         for i in range(num_hits):
             symbol = BarCodeSymbol()
             symbol.data = str(self._dm_read.message(i))
             symbol.type = str(self._dm_read.stats(i))
             self.symbols.append(symbol)
+        self.symbols = BarCodeSymbol.quality_sorted( self.symbols )
         return len(self.symbols)
+
+
+
+class BarCode(AdPythonPlugin):
+    self.SCANNER_ALL= 0
+    self.SCANNER_DMTX = 1
+    self.SCANNER_ZBAR = 2
+
+    def __init__(self):
+        self.log.setLevel(logging.DEBUG)
+        params = dict(data = "", type = "", count = 0, quality = 0, threshold = 10, 
+                      busy = 0, scanner = self.SCANNER_ALL)
+        AdPythonPlugin.__init__(self, params)
+        self.scanners = { 'zbar': ZBarDecoder(), 'dmtx': DmtxDecoder() }
+        self._busy = 0
+        
+    def paramChanged(self):
+        # Check if user has made busy record busy
+        if self['busy'] == 1 and (self['busy'] != self._busy):
+            self.log.debug('Starting scan!')
+            self._busy = 1
+            self['count'] = 0
+            self['data'] = ""
+            self['type'] = ""
+            self['quality'] = 0
+    
+    def processArray(self, arr, attr):
+        # If the user has not started scanning then abort immidiately
+        if not self['busy'] == 1: return None
+        
+        # Work out which of the decoders to use - or all of them
+        if self['scanner'] == self.SCANNER_ALL: 
+            decoders = self.scanners.values()
+        else: 
+            decoders = [self.scanners[self['scanner']]]
+        
+        # Run through the decoders and check if they find barcodes.
+        # Break out of the loop as soon as a decoder has a hit
+        count = 0
+        for decoder in decoders:
+            count = decoder.decode( arr, threshold = self['threshold'] )
+            if count > 0: break
+        
+        # Abort if no barcode was found
+        if count == 0: return None
+        
+        self.log.debug('Found symbols: %s', decoder.symbols)
+        # Find the best quality result of the scan
+        symbol = decoder.symbols[0]
+        
+        # Abort if the quality of the scan was below desired threshold
+        if symbol.quality < self['threshold']:
+            self.log.warning('The detection quality was below threshold (%d < %d) (False match) %s', 
+                             symbol.quality, self['threshold'], str(decoder))
+            return None
+        
+        # Take a copy of the input array in order to return a new array
+        dest = numpy.array(arr)
+        
+        # If a polygon of the scan position of the barcode is available, draw it
+        # on top of the output array
+        if len(symbol.polygon) > 0:
+            self.log.debug("Drawing polygon on output: %s", str(dest.shape))
+            cv2.polylines(dest, [symbol.polygon], True, 0, 5)
+        
+        return dest
+        
 
 class Zbar(AdPythonPlugin):
     def __init__(self):
