e0784378b646880e83390ce8295fb92a2773a806
Ulrik Pedersen <Ulrik.Pedersen@diamond.ac.uk>
2013-10-31 11:44:45 +0000

    adPythonZBar: first go at making a generic barcode decoder class with subclasses for zbar and dmtx.

--- adPythonApp/scripts/adPythonZbar.py
+++ adPythonApp/scripts/adPythonZbar.py
@@ -3,10 +3,84 @@ from adPythonPlugin import AdPythonPlugin
 import cv2
 import numpy
 import sys
+import logging
 
 sys.path.append('/dls_sw/work/tools/RHEL6-x86_64/zbar/prefix/lib/python2.7/site-packages')
 import zbar
-import logging
+
+sys.path.append('/dls_sw/work/tools/RHEL6-x86_64/pydmtx/prefix/lib/python2.7/site-packages')
+import pydmtx
+
+class BarCodeSymbol:
+    def __init__(self):
+        self.data = ''
+        self.type = ''
+        self.polygon = []
+        self.quality = 0
+        
+    def __str__(self):
+        s = "<BarCodeSymbol type=\'%s\', data=\'%s\', quality=%d >" % (self.type, self.data, self.quality)
+        return s
+    
+    def points_to_polygon(self, points):
+        '''Takes a set of points in lists or tuples: ((x1,y1), (x2,y2), ... (xn,yn))
+        and converts it into a shape that OpenCV can treat as points in a polygon.'''
+        points = numpy.array(points, numpy.int32)
+        self.polygon = points.reshape((-1, 1, 2))
+        
+class BarCodeDecoder:
+    '''BarDecoder base class to provide a common interface for 
+    implementing various barcode libraries decode support'''
+    def __init__(self, logger='BarCodeDecoder'):
+        self.log = logging.getLogger(logger)
+        self.symbols = []
+        
+    def __str__(self):
+        s = "< BarDecoder symbols: %s >" % str(self.symbols)
+        return s
+        
+    def decode(self, array):
+        '''Decode the barcode symbols from a 2D array
+        The input array should be a numpy 2D array of unsigned 8 bpp elements.
+        Returns the number of symbols decoded from the array.'''
+        raise NotImplementedError
+
+class ZBarDecoder(BarCodeDecoder):
+    '''Use ZBar to decode a range of barcode types'''
+    def __init__(self, logger='ZBarDecoder'):
+        BarCodeDecoder.__init__(self, logger)
+        self._scanner = zbar.ImageScanner()
+        self._scanner.parse_config('enable')
+    
+    def decode(self, array):
+        self.symbols = []
+        zimg = zbar.Image( array.shape[0], array.shape[1], 'Y800', array.tostring() )
+        self._scanner.scan(zimg)
+        for zb_symbol in zimg:
+            symbol = BarCodeSymbol()
+            symbol.type = str(zb_symbol.type)
+            symbol.data = zb_symbol.data
+            symbol.quality = int(zb_symbol.quality)
+            symbol.points_to_polygon(zb_symbol.location)
+            self.symbols.append(symbol)
+        return len(self.symbols)
+
+class DmtxDecoder(BarCodeDecoder):
+    '''Use the pydmtx - python bindings for libdmtx to decode Data Matrix barcodes'''
+    def __init__(self, logger='DmtxDecoder'):
+        BarCodeDecoder.__init__(self, logger)
+        self._dm_read = pydmtx.DataMatrix()
+        
+    def decode(self, array):
+        self.symbols = []
+        self._dm_read.decode( array.shape[0], array.shape[1], buffer(array))
+        num_hits = self._dm_read.count()
+        for i in range(num_hits):
+            symbol = BarCodeSymbol()
+            symbol.data = str(self._dm_read.message(i))
+            symbol.type = str(self._dm_read.stats(i))
+            self.symbols.append(symbol)
+        return len(self.symbols)
 
 class Zbar(AdPythonPlugin):
     def __init__(self):
